617ac817bd9b6abf4a51e2bbc08df23eb85902ad
Eugene Korniets <evgeniy.korniets@gmail.com>
2019-12-16 00:04:15 +0300

    fix render Board component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,11 +6,34 @@ import Square from '../Square/Square'
 
 class Board extends React.Component {
   render () {
+    const status = 'Next player: X'
+
     return (
       <div className="board">
         <p className="board__title">Board</p>
-        <Square/>
+        <p className="board__status">{ status }</p>
+        <div className="board__row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board__row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board__row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
+    )
+  }
+
+  renderSquare (i) {
+    return (
+      <Square value={i}/>
     )
   }
 }
