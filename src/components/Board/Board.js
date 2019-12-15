4d1635c725cd3e22cbe697801cc76fb95ad43ad5
Eugene Korniets <evgeniy.korniets@gmail.com>
2019-12-16 01:01:05 +0300

    add state to Board component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -5,8 +5,16 @@ import './Board.css'
 import Square from '../Square/Square'
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      isXNext: true
+    }
+  }
+
   render () {
-    const status = 'Next player: X'
+    const status = `Следующий ход: ${this.state.isXNext ? 'X' : 'O'}`
 
     return (
       <div className="board">
@@ -33,8 +41,22 @@ class Board extends React.Component {
 
   renderSquare (i) {
     return (
-      <Square value={i}/>
+      <Square
+        value={ this.state.squares[i] }
+        onClick={
+          () => this.handleClick(i)
+        }
+      />
     )
+  }
+
+  handleClick (i) {
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.isXNext ? 'X' : 'O'
+    this.setState({
+      squares,
+      isXNext: !this.state.isXNext
+    })
   }
 }
 
